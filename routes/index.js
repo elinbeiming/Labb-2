e9d71f9274bd90cfebed2f297551f15be1a1fd58
elinbeiming <46751847+elinbeiming@users.noreply.github.com>
2019-10-04 14:33:33 +0200

    move

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+let store = {
+  post: [
+    {name: 'Top 10 ES6 Featuers every Wed Developer must know',
+    url: 'https://webapplog.com/es6',
+    text: 'This essay will give you a quick introduction to ES6. If you don`t know what is ES6, it`s a new JavaScript implementation.',
+    comments: [
+      text: 'Cruel.....var {house,mouse} = No type optimazation at all',
+      text: 'I think you`re undervaluing the benefit of ´let` and ´const`.',
+      text: '(p1,p2)=>{...} ,i understand this ,thank you!'
+    ]
+    }
+  ]
+}
