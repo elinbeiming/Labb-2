36e71699bce6afdf510b6b1fb6a539fc999e605a
elinbeiming <46751847+elinbeiming@users.noreply.github.com>
2019-10-09 14:32:03 +0200

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,4 @@
-let store = {
-  post: [
-    {name: 'Top 10 ES6 Featuers every Wed Developer must know',
-    url: 'https://webapplog.com/es6',
-    text: 'This essay will give you a quick introduction to ES6. If you don`t know what is ES6, it`s a new JavaScript implementation.',
-    comments: [
-      text: 'Cruel.....var {house,mouse} = No type optimazation at all',
-      text: 'I think you`re undervaluing the benefit of ´let` and ´const`.',
-      text: '(p1,p2)=>{...} ,i understand this ,thank you!'
-    ]
-    }
-  ]
+module.exports = {
+    posts: require('./posts.js'),
+    comments: require('./comments.js')
 }
--- routes/post.js
+++ routes/post.js
@@ -1,15 +1,17 @@
-module.export = {
-  getPost(req, res) {
-
-  },
-  addPost(req, res) {
-
-  },
-  updatePost(req, res) {
-
-  },
-  removePost(req, res) {
-
-  }
+module.exports = {
+    getPosts (req,res){
+        res.status(200).json(req.store.posts)
+    },
+    addPost (req,res){
+        req.store.posts.push(req.body)
+        res.status(201).send({id : req.store.posts.length-1})
+    },
+    updatePost (req,res){
+        req.store.posts[req.params.postId] = req.body
+        res.status(200).send(req.store.posts[req.params.postId])
+    },
+    deletePost (req,res){
+        req.store.posts.splice(req.params.postId,1)
+        res.sendStatus(204)
+    }
 }
-//Jag vet inte vad jag gör
